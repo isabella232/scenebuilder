fce7106eb3676e439868a73da62fe7ad76757e5e
Anna Kozlova <anna.kozlova@jetbrains.com>
2020-06-10 20:29:53 +0200

    pack all required javafx compilation dependencies together with scenebuilder (take II)

--- kit/src/main/java/org/jetbrains/plugins/javaFX/sceneBuilder/JavaFXPlatformHelper.java
+++ kit/src/main/java/org/jetbrains/plugins/javaFX/sceneBuilder/JavaFXPlatformHelper.java
@@ -6,6 +6,9 @@ import com.oracle.javafx.scenebuilder.kit.editor.panel.content.ContentPanelContr
 import com.oracle.javafx.scenebuilder.kit.editor.panel.hierarchy.treeview.HierarchyTreeViewController;
 import com.oracle.javafx.scenebuilder.kit.editor.panel.inspector.InspectorPanelController;
 import com.oracle.javafx.scenebuilder.kit.editor.panel.library.LibraryPanelController;
+import com.oracle.javafx.scenebuilder.kit.library.BuiltinLibrary;
+import com.oracle.javafx.scenebuilder.kit.library.Library;
+import com.oracle.javafx.scenebuilder.kit.library.LibraryItem;
 import javafx.application.Platform;
 import javafx.beans.value.ChangeListener;
 import javafx.embed.swing.JFXPanel;
@@ -16,6 +19,11 @@ import javafx.scene.SceneAntialiasing;
 import javafx.scene.control.SplitPane;
 
 import javax.swing.*;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 @SuppressWarnings("unused")
 public class JavaFXPlatformHelper {
@@ -76,6 +84,79 @@ public class JavaFXPlatformHelper {
       if (selectionListener != null) {
         editorController.getSelection().revisionProperty().removeListener((ChangeListener<Number>)selectionListener);
       }
+    }
+  }
+
+  public static class CustomComponent {
+    private final String myName;
+    private final String myQualifiedName;
+    private final String myModule;
+    private final Map<String, String> myAttributes;
+
+    public CustomComponent(String name,
+                           String qualifiedName,
+                           String module,
+                           Map<String, String> attributes) {
+      myName = name;
+      myQualifiedName = qualifiedName;
+      myModule = module;
+      myAttributes = attributes;
+    }
+
+    public String getDisplayName() {
+      return myModule != null && !myModule.isEmpty() ? myName + " (" + myModule + ")" : myName;
+    }
+
+    public String getFxmlText() {
+      final StringBuilder builder =
+              new StringBuilder(String.format("<?xml version=\"1.0\" encoding=\"UTF-8\"?><?import %s?><%s", myQualifiedName, myName));
+      myAttributes.forEach((name, value) -> builder.append(String.format(" %s=\"%s\"", name, value.replace("\"", "&quot;"))));
+      builder.append("/>");
+      return builder.toString();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+      if (this == o) return true;
+      if (!(o instanceof CustomComponent)) return false;
+
+      CustomComponent c = (CustomComponent)o;
+      return myQualifiedName.equals(c.myQualifiedName) && Objects.equals(myModule, c.myModule);
+    }
+
+    @Override
+    public int hashCode() {
+      return Objects.hash(myQualifiedName, myModule);
+    }
+
+    @Override
+    public String toString() {
+      return myModule != null ? myQualifiedName + "(" + myModule + ")" : myQualifiedName;
+    }
+  }
+
+  public static class CustomLibrary extends Library {
+    private static final String CUSTOM_SECTION = "Custom";
+
+    public CustomLibrary(ClassLoader classLoader, List<CustomComponent> customComponents) {
+      classLoaderProperty.set(classLoader);
+
+      getItems().setAll(BuiltinLibrary.getLibrary().getItems());
+      final List<LibraryItem> items = customComponents.stream().map(component -> new LibraryItem(component.getDisplayName(), CUSTOM_SECTION, component.getFxmlText(), null, this)).collect(Collectors.toList());
+      getItems().addAll(items);
+    }
+
+    @Override
+    public Comparator<String> getSectionComparator() {
+      return CustomLibrary::compareSections;
+    }
+
+    private static int compareSections(String s1, String s2) {
+      final boolean isCustom1 = CUSTOM_SECTION.equals(s1);
+      final boolean isCustom2 = CUSTOM_SECTION.equals(s2);
+      if (isCustom1) return isCustom2 ? 0 : 1;
+      if (isCustom2) return -1;
+      return BuiltinLibrary.getLibrary().getSectionComparator().compare(s1, s2);
     }
   }
 }
